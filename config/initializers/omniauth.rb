f56d4f33b49681f6801bbd1ea3297f6a88b44d3a
emgord <emgord@gmail.com>
2016-02-23 12:25:25 -0800

    working on oauth ssl error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
+    :scope => 'email,profile',
+    :access_type => 'online',
+    :prompt => ''
+  }
 end
