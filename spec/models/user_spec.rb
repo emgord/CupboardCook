c8fb7d22b41486abcc6ffa582dc08590c0e571e5
emgord <emgord@gmail.com>
2016-02-24 15:19:26 -0800

    wrote tests for find recipe method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,4 +41,16 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe ".find_recipes" do
+    let(:empty_pantry_user) {create(:google_user)}
+    let(:full_pantry_user) {create(:google_user, :full_pantry)}
+    let(:recipe) {create(:recipe)}
+    it "returns recipes you can make using ingredients in your pantry" do
+      expect(full_pantry_user.find_recipes).to_include(recipe)
+    end
+    it "returns nothing if you can't make any recipes" do
+      expect(empty_pantry_user.find_recipes.length).to eq(0)
+    end
+  end
 end
