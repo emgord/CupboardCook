26798bdfcd9750fcaa7ebef45c33dab801ed86ba
emgord <emgord@gmail.com>
2016-02-23 16:20:46 -0800

    google oauth working, need to handle auth hash

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'omniauth'
 gem "omniauth-google-oauth2"
+gem 'omniauth-oauth2', '~> 1.3.1'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-    :scope => 'email,profile',
-    :access_type => 'online',
-    :prompt => ''
+    :name => "google",
+    # :access_type => "online",
+    :client_options => { :ssl => { :verify => false } }
   }
 end
