03f12a42337363cd91b9201507904237e9e783b4
emgord <emgord@gmail.com>
2016-02-25 09:54:44 -0800

    added relationships so ingredients connect recipes and users, added seeds so user can make one recipe

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,7 @@
 class Ingredient < ActiveRecord::Base
+  has_many :user_ingredients
+  has_many :users, through: :user_ingredients
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
   validates :name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -479,7 +479,34 @@ end
 
 seed_user_ingredients = [
   { user_id:1,
+    ingredient_id:1
+  },
+  { user_id:1,
     ingredient_id:2
+  },
+  { user_id:1,
+    ingredient_id:3
+  },
+  { user_id:1,
+    ingredient_id:4
+  },
+  { user_id:1,
+    ingredient_id:5
+  },
+  { user_id:1,
+    ingredient_id:6
+  },
+  { user_id:1,
+    ingredient_id:7
+  },
+  { user_id:1,
+    ingredient_id:8
+  },
+  { user_id:1,
+    ingredient_id:9
+  },
+  { user_id:1,
+    ingredient_id:10
   }
 ]
 
