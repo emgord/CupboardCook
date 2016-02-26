4a2786d3d6e90aea2d5a924e4a74407213c9c8af
emgord <emgord@gmail.com>
2016-02-26 10:58:19 -0800

    minor style for recipe show

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -1,8 +1,17 @@
 class UserIngredientsController < ApplicationController
   before_action :require_user
 
+  def create
+
+
+  end
+
   def index
+    @item = UserIngredient.new
     @pantry_items = current_user.ingredients.all
   end
+
+  private
+
 
 end
