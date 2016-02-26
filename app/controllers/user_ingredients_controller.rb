2aa3bafb472da5347d5308ced3fd9db6b29f5a6d
emgord <emgord@gmail.com>
2016-02-26 12:00:47 -0800

    user can add ingredients to pantry

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -2,16 +2,30 @@ class UserIngredientsController < ApplicationController
   before_action :require_user
 
   def create
-
-
+    @item = UserIngredient.new(amount_params)
+    @item.ingredient_id = item_params
+    @item.user_id = current_user.id
+    if @item.save
+      redirect_to root_path
+    else
+      render index
+    end
   end
 
   def index
     @item = UserIngredient.new
+    @ingredients = Ingredient.all
     @pantry_items = current_user.ingredients.all
   end
 
   private
+
+  def amount_params
+    params.require(:user_ingredient).permit(:amount,:unit)
+  end
+  def item_params
+    params.require(:ingredient)
+  end
 
 
 end
