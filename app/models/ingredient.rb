3b89638e75db75f60514f9451702dc6ea484fabf
emgord <emgord@gmail.com>
2016-02-25 16:37:11 -0800

    added update ingredient count and update ingredients counts for recipes

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,5 @@ class Ingredient < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
   validates :name, presence: true
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,14 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
+  def update_ingredient_count
+    self.ingredient_count = self.ingredients.length
+    self.save
+  end
+
+  def self.update_ingredient_counts
+    Recipe.all.each do |recipe|
+      recipe.update_ingredient_count
+    end
+  end
 end
