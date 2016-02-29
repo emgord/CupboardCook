666d121c8f8e4738ac1bde44801bc5d8d6f16cd8
emgord <emgord@gmail.com>
2016-02-29 11:32:05 -0800

    click renders addForm variable

--- app/assets/javascripts/components/Ingredient.js.jsx
+++ app/assets/javascripts/components/Ingredient.js.jsx
@@ -1,10 +1,25 @@
 var Ingredient = React.createClass({
+	getInitialState() {
+		return { add:false };
+	},
+	showAddForm(){
+		this.setState({ add:true});
+	},
+
 	render () {
+		var add = this.state.add;
+		var addForm;
+		if (add) {
+			addForm = <p>Form</p>;
+		}
 		return (
-			<div className='btn-group'>
+			<div className='btn-group ingredient-add'>
 				<p className= "btn btn-default">{ this.props.name }</p>
-	      <button className="btn btn-default" type="button"><i className="fa fa-plus"></i></button>
+	      <button className="btn btn-default" type="button" onClick={this.showAddForm}><i className="fa fa-plus"></i></button>
+				{addForm}
 			</div>
+
 			)
-	}
+	},
+
 });
