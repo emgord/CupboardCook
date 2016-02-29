54dff7868f1ee4714fe59e0ff081eeed4f350409
emgord <emgord@gmail.com>
2016-02-29 09:40:41 -0800

    new style for ingredient buttons

--- app/assets/javascripts/components/Ingredient.js.jsx
+++ app/assets/javascripts/components/Ingredient.js.jsx
@@ -1,8 +1,9 @@
 var Ingredient = React.createClass({
 	render () {
 		return (
-			<div className='btn btn-default ingredient-add'>
-			  { this.props.name } <i className="fa fa-plus-square-o fa-lg"></i>
+			<div className='btn-group'>
+				<p className= "btn btn-default">{ this.props.name }</p>
+	      <button className="btn btn-default" type="button"><i className="fa fa-plus"></i></button>
 			</div>
 			)
 	}
