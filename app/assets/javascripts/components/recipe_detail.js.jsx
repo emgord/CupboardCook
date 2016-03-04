78d4e93fb17ea240d8bdaf0d57d3085ea02ae615
emgord <emgord@gmail.com>
2016-03-03 17:04:18 -0800

    recipe detail shows recipes you have" git push

--- app/assets/javascripts/components/recipe_detail.js.jsx
+++ app/assets/javascripts/components/recipe_detail.js.jsx
@@ -12,10 +12,12 @@ var RecipeDetail = React.createClass({
           <img src={image} />
           <div className="caption">
           <h2>{this.props.recipe.title}</h2>
+          <h3>Ingredients:</h3>
+          <Ingredients ingredients={this.props.recipe.ingredients} />
           <p>Time: {this.props.recipe.time}</p>
           <p>Yield: {this.props.recipe.yield}</p>
           <p>{this.props.recipe.description}</p>
-          <a href={this.props.recipe.original_url}>Cook Now</a>
+          <a href={this.props.recipe.original_url} target="_blank">Cook This Now</a>
           </div>
         </div>
       </div>
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = current_user.find_recipes
+    @recipes = current_user.find_recipes.as_json(:except => [:create_at, :updated_at],
+                                                            :include => {:ingredients => {:only => [:name, :id]}} )
   end
 end
