7e6aceb5aa5b065e9a2a524208b761294481f82b
emgord <emgord@gmail.com>
2016-03-04 11:28:38 -0800

    working on showing user ingredient if you have it

--- app/assets/javascripts/components/ingredients.js.jsx
+++ app/assets/javascripts/components/ingredients.js.jsx
@@ -2,13 +2,19 @@ var Ingredients = React.createClass({
 
 	render: function () {
 		var showIngredients = this.props.ingredients.map(function(ingredient){
-			return( <Ingredient name={ingredient.name}
-									id={ingredient.id}
-									key={ingredient.id}
-									addUserIngredient={this.props.addUserIngredient}
-									resetSearch={this.props.resetSearch}
-								/>
-		);}, this);
+			this.props.user_ingredients.map(function(user_ingredient){
+				if (user_ingredient.ingredient_id == ingredient.id) {
+						return( <div>You have it!</div> );
+				} else {
+					return( <Ingredient name={ingredient.name}
+											id={ingredient.id}
+											key={ingredient.id}
+											addUserIngredient={this.props.addUserIngredient}
+											resetSearch={this.props.resetSearch}/>
+									 );
+				}
+			});
+			}, this);
 
 		return(
 			<div>{showIngredients}</div>
