10c4f6662be6209c423817682727236217a543e1
emgord <emgord@gmail.com>
2016-03-06 21:47:33 -0800

    commented out after save on recipes to update ingredient count bc it is breaking some tests, ingredient count just updated when new join table is made

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,8 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
-  after_save do
-    update_column(:ingredient_count, self.ingredients.length)
-  end
+  # after_save do
+  #   update_column(:ingredient_count, self.ingredients.length)
+  # end
 
 end
