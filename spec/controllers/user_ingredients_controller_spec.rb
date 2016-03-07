0d44645fb57b288a0eb0aa5f91d379406fa2b78e
emgord <emgord@gmail.com>
2016-03-06 16:16:21 -0800

    working on controller specs

--- spec/controllers/user_ingredients_controller_spec.rb
+++ spec/controllers/user_ingredients_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe UserIngredientsController, type: :controller do
 
+  context "user is logged out" do
+
+  end
+
+  context "user is logged in" do
+    before :each do
+      @user = create(:user, :google)
+      session[:user_id] = @user.id
+    end
+  end
+
+
 end
