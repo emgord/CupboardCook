68a612e773074067bd438390c201d5d2ef8976d1
emgord <emgord@gmail.com>
2016-03-09 15:39:45 -0800

    ingredient validations and find or create method added for ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,6 +3,21 @@ class Ingredient < ActiveRecord::Base
   has_many :users, through: :user_ingredients
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 40 }
   searchkick
+
+  def self.find_or_create(ingredient_name_string)
+    ingredient_name_string.downcase!
+    ingredient = self.find_by(name: ingredient_name_string)
+    if !ingredient.nil?
+      return ingredient
+    else
+      ingredient = Ingredient.new(name: ingredient_name_string)
+      if ingredient.save
+        return ingredient
+      else
+        return nil
+      end
+    end
+  end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -5,5 +5,40 @@ RSpec.describe Ingredient, type: :model do
     it "must have a name" do
       expect(Ingredient.new(name: nil)).to_not be_valid
     end
+    it "name must be unique" do
+      Ingredient.create(name:"a")
+      expect(Ingredient.new(name: "a")).to_not be_valid
+    end
+    it "name can have 40 characters" do
+      expect(Ingredient.new(name: "a" * 40)).to be_valid
+    end
+    it "name cannot have 41 characters" do
+      expect(Ingredient.new(name: "a" * 41)).to_not be_valid
+    end
+  end
+
+  describe ".find_or_create" do
+    it "creates an ingredient if it does not already exist" do
+      new_ingredient = Ingredient.find_or_create("tomato")
+      expect(new_ingredient).to be_valid
+      expect(Ingredient.all.length).to eq 1
+    end
+    it "downcases the ingredient name" do
+      new_ingredient = Ingredient.find_or_create("Tomato")
+      expect(new_ingredient).to be_valid
+      expect(Ingredient.all.length).to eq 1
+      expect(new_ingredient.name).to eq "tomato"
+    end
+    it "finds and returns the ingredient if it already exists" do
+      Ingredient.find_or_create('tomato')
+      ingredient = Ingredient.find_or_create('Tomato')
+      expect(ingredient).to be_valid
+      expect(Ingredient.all.length).to eq 1
+    end
+    it "returns nil if the ingredient is not valid" do
+      ingredient = Ingredient.find_or_create("a" * 55)
+      expect(ingredient).to eq nil
+      expect(Ingredient.all.length).to eq 0
+    end
   end
 end
