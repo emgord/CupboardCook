5cd80c7bd5ad971ef053c05d4a7dc1a5087ad0f5
emgord <emgord@gmail.com>
2016-03-09 20:26:14 -0800

    validation that recipe can have each ingredient only once, breaking some other tests

--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe RecipeIngredient, type: :model do
+  describe ".validates" do
+    it "must have recipe_id and ingredient id" do
+        recipe_ingredient = build(:recipe_ingredient)
+        expect(recipe_ingredient).to be_valid
+    end
+    it "must have a ingredient_id" do
+      recipe_ingredient = build(:recipe_ingredient, ingredient_id: nil)
+      expect(recipe_ingredient).to_not be_valid
+    end
+    it "must have a recipe_id" do
+      recipe_ingredient = build(:recipe_ingredient, recipe_id: nil)
+      expect(recipe_ingredient).to_not be_valid
+    end
+    it "recipe cannot have duplicate ingredients" do
+      recipe_ingredient = create(:recipe_ingredient)
+      second = RecipeIngredient.new(recipe_id: recipe_ingredient.recipe_id, ingredient_id: recipe_ingredient.ingredient_id)
+      expect(second).to_not be_valid
+    end
+  end
 end
