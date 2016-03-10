59d1e47e124faa618080ff0897f57da2dd271c34
emgord <emgord@gmail.com>
2016-03-09 16:25:25 -0800

    added validation for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,14 @@
 class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+  validates :title, presence: true, length: { maximum: 150 }
+  validates :yield, length: { maximum: 30 }
+  validates :time, length: { maximum: 30 }
+  validates :original_url, presence: true, uniqueness:true, length: {maximum: 250}
+  validates :image, length: {maximum: 300}
+  validates :description, length: {maximum: 2000}
+
+
 
   # after_save do
   #   update_column(:ingredient_count, self.ingredients.length)
@@ -18,7 +26,7 @@ class Recipe < ActiveRecord::Base
       if recipe.save
         recipe_hash["ingredients"].each do |ingredient|
           new_ingredient = Ingredient.find_or_create(ingredient)
-          if !new_ingredient.nil?
+          if !new_ingredient.nil? && !new_ingredient.salt_or_pepper?
             recipe.ingredients << new_ingredient
           end
         end
