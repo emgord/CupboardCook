f8fe9b7b3a1f7c365c437c128dbceced9b54802b
Emily Gordon <emgord@gmail.com>
2016-03-12 13:43:42 -0800

    Revert "Recipepop"

--- app/assets/javascripts/components/recipe_detail.js.jsx
+++ app/assets/javascripts/components/recipe_detail.js.jsx
@@ -13,10 +13,12 @@ var RecipeDetail = React.createClass({
     if (image == null) {
       image = "http://thecrites.com/sites/all/modules/cookbook/theme/images/default-recipe-big.png"
     }
+    var show = this.props.show ? 'recipe-detail shift' : 'recipe-detail hide';
 
     return(
 
-        <div className="thumbnail recipe-detail">
+      <div className = {show} >
+        <div className="thumbnail">
         <a onClick={this.props.hide}><i className="fa fa-times-circle fa-pull-right fa-2x"></i></a>
           <img src={image} />
           <div className="caption">
@@ -33,6 +35,7 @@ var RecipeDetail = React.createClass({
           <a className="btn btn-info" onClick={this.markRecipeComplete}>Cooked!</a>
           </div>
         </div>
+      </div>
     );
   }
 
