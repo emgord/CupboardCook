7d19e3f2670018e09c480b6402553781ec0f567c
emgord <emgord@gmail.com>
2016-03-14 13:44:59 -0700

    tests for specifying number of missing ingredients

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,13 +45,26 @@ RSpec.describe User, type: :model do
 
   describe ".find_recipes" do
     let(:user_with_all_ingred) {create(:user_with_all_ingred)}
-    let(:user_missing_ingreds) {create(:user_missing_ingreds)}
+    let(:user_missing_1_ingred) {create(:user_missing_1_ingred)}
+    let(:user_missing_2_ingred) {create(:user_missing_2_ingred)}
     it "returns recipes you can make using ingredients in your pantry" do
       expect(user_with_all_ingred.find_recipes.count).to eq(1)
       expect(user_with_all_ingred.find_recipes[0]).to eq(Recipe.first)
     end
     it "returns nothing if you can't make any recipes" do
-      expect(user_missing_ingreds.find_recipes.length).to eq(0)
+      expect(user_missing_1_ingred.find_recipes.count).to eq(0)
+    end
+    it "can specify to include recipes missing 1 ingredient" do
+      expect(user_missing_1_ingred.find_recipes(1).count).to eq(1)
+      expect(user_with_all_ingred.find_recipes(1)[0]).to eq(Recipe.first)
+    end
+    it "can specify to include recipes missing 2 ingredients" do
+      expect(user_missing_2_ingred.find_recipes(2).count).to eq(1)
+      expect(user_missing_2_ingred.find_recipes(2)[0]).to eq(Recipe.first)
+    end
+    it "still returns the recipe even if you have all ingredients" do
+      expect(user_with_all_ingred.find_recipes(1).count).to eq(1)
+      expect(user_with_all_ingred.find_recipes(1)[0]).to eq(Recipe.first)
     end
   end
 end
