a6e9a0493b6cf8fe5a712445059eed019324bc27
emgord <emgord@gmail.com>
2016-03-13 20:24:38 -0700

    style for recipe detail page

--- app/assets/javascripts/components/recipe_detail.js.jsx
+++ app/assets/javascripts/components/recipe_detail.js.jsx
@@ -9,30 +9,57 @@ var RecipeDetail = React.createClass({
   },
 
   render: function(){
-    var image = <img src={this.props.recipe.image} />
     if (this.props.recipe.image == null) {
-      image = "";
+      var image = "";
+    } else {
+      var image = <img src={this.props.recipe.image} className="img-responsive center-block"/>;
+    }
+
+    if (this.props.recipe.time == null) {
+      var time = "";
+    } else {
+      var time = <span><i className="fa fa-clock-o">   </i><strong>Time:</strong>{this.props.recipe.time}</span>;
+    }
+
+    if (this.props.recipe.time == null) {
+      var recipeYield = "";
+    } else {
+      var recipeYield = <span><i className="fa fa-cutlery">   </i><strong>Yield:</strong>{this.props.recipe.yield}</span>;
     }
 
     return(
-
-        <div className="recipe-detail thumbnail">
-        <a onClick={this.props.hide}><i className="fa fa-times-circle fa-pull-right fa-2x"></i></a>
-          {image}
-          <div className="caption">
-          <h2>{this.props.recipe.title}</h2>
-          <h3>Ingredients:</h3>
-          <Ingredients ingredients={this.props.recipe.ingredients}
-                       userIngredients={this.props.user_ingredients}
-                       removeUserIngredient={this.props.removeUserIngredient}
-                       editUserIngredient={this.state.completeRecipe}/>
-          <p>Time: {this.props.recipe.time}</p>
-          <p>Yield: {this.props.recipe.yield}</p>
-          <p>{this.props.recipe.description}</p>
-          <a className="btn btn-primary" href={this.props.recipe.original_url} target="_blank">Cook This Now</a>
-          <a className="btn btn-info" onClick={this.markRecipeComplete}>Cooked!</a>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="recipe-detail col-xs-12 col-sm-8 col-sm-push-2">
+              <div className="panel panel-default">
+                <div className="panel-heading">
+                  <a onClick={this.props.hide}><i className="fa fa-times fa-pull-right fa-border fa-2x"></i></a>
+                  <h2>{this.props.recipe.title}</h2>
+                </div>
+                <div className="panel-body">
+                {image}
+                <div className="time-yield-group">
+                  {time}
+                  {recipeYield}
+                </div>
+                <h3>Ingredients:</h3>
+                <Ingredients ingredients={this.props.recipe.ingredients}
+                             userIngredients={this.props.user_ingredients}
+                             removeUserIngredient={this.props.removeUserIngredient}
+                             editUserIngredient={this.state.completeRecipe}/>
+                <p>{this.props.recipe.description}</p>
+                </div>
+                <div className="recipe-actions panel-footer">
+                  <a className="btn btn-primary" href={this.props.recipe.original_url} target="_blank">Cook This Now <i className="fa fa-external-link"></i></a>
+                  <a className="btn btn-info" onClick={this.markRecipeComplete}>Cooked <i className="fa fa-check"></i></a>
+                  <a><i className="fa fa-heart fa-4x"></i></a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
     );
   }
 
