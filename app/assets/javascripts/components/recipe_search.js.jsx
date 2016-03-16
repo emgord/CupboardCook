23528cc5cf3fb70ea2bba64c3fdb82240ae18843
emgord <emgord@gmail.com>
2016-03-16 14:01:02 -0700

    view toggles buttons for heart vs unheart

--- app/assets/javascripts/components/recipe_search.js.jsx
+++ app/assets/javascripts/components/recipe_search.js.jsx
@@ -27,19 +27,39 @@ var RecipeSearch = React.createClass({
     this.setState({heart: heart, missing:missing }, this.updateSearch)
   },
 
+  showAll: function(){
+    var heart = false;
+    var missing = 0;
+    this.setState({heart: heart, missing:missing }, this.updateSearch)
+  },
+
   updateSearch: function(){
     var search_options = this.state;
     this.props.resetRecipes(search_options)
   },
 
 	render () {
+
+    if (this.state.heart) {
+      var sortOptions = <a onClick={this.showAll} className="btn btn-primary">Unheart!</a>;
+    } else {
+      var sortOptions =
+          <div>
+      			<div className="btn-group recipe-search">
+              <a onClick={this.decrementMissing} className="btn btn-primary"><i className="fa fa-chevron-left"></i></a>
+              <p className= "btn btn-default">{this.state.missing}</p>
+              <a onClick={this.incrementMissing} className="btn btn-primary"><i className="fa fa-chevron-right"></i></a>
+            </div>
+            <a onClick={this.showHeart} className="btn btn-primary">Heart!</a>
+            </div>;
+    }
+
+
 		return (
-			<div className="btn-group recipe-search">
-        <a onClick={this.decrementMissing} className="btn btn-primary"><i className="fa fa-chevron-left"></i></a>
-        <p className= "btn btn-default">{this.state.missing}</p>
-        <a onClick={this.incrementMissing} className="btn btn-primary"><i className="fa fa-chevron-right"></i></a>
-        <a onClick={this.showHeart} className="btn btn-primary">Heart!</a>
-			</div>
+        <div className = "recipe-search-box">
+          {sortOptions}
+        </div>
+
 			);
 	}
 });
