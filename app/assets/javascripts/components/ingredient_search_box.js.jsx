a90d09f0408f9a4b3255d1247be08885cb3d4e53
emgord <emgord@gmail.com>
2016-03-17 14:19:42 -0700

    change style for search

--- app/assets/javascripts/components/ingredient_search_box.js.jsx
+++ app/assets/javascripts/components/ingredient_search_box.js.jsx
@@ -3,15 +3,19 @@ var IngredientSearchBox = React.createClass({
 	render () {
 		return (
 			<div className="ingredient-search-box">
-				<form className="uk-search" ref="form" action={ this.props.searchPath } acceptCharset="UTF-8" method="get">
-				<p><input ref="query"
-									className="uk-search-field"
-					 				autoFocus="true"
-									value={ this.props.query }
-									name="query"
-									placeholder="Find Ingredients"
-									onChange={ this.props.submitPath }
-									/></p>
+				<form className="uk-form" ref="form" action={ this.props.searchPath } acceptCharset="UTF-8" method="get">
+					<div className="uk-form-icon">
+						<i className="uk-icon-search"></i>
+						<input ref="query"
+											type="text"
+											className="uk-form-large uk-form-width-large"
+							 				autoFocus="true"
+											value={ this.props.query }
+											name="query"
+											placeholder="Find Ingredients"
+											onChange={ this.props.submitPath }
+											/>
+					</div>
 				</form>
 			</div>
 			);
--- app/assets/javascripts/components/recipe_search_box.js.jsx
+++ app/assets/javascripts/components/recipe_search_box.js.jsx
@@ -3,14 +3,17 @@ var RecipeSearchBox = React.createClass({
 	render () {
 		return (
 			<div className="recipe-search-box">
-				<form className="uk-search" ref="form" acceptCharset="UTF-8">
-				<p><input ref="query"
-									className="uk-search-field"
-					 				autoFocus="true"
-									name="query"
-                  onChange={ this.props.updateQuery }
-									placeholder="Find Recipes"
-									/></p>
+				<form className="uk-form" ref="form" acceptCharset="UTF-8">
+					<div className="uk-form-icon">
+						<i className="uk-icon-search"></i>
+						<input ref="query"
+									 className="uk-form-large uk-form-width-large"
+							 		 autoFocus="true"
+									 name="query"
+		               onChange={ this.props.updateQuery }
+									 placeholder="Find Recipes"
+										/>
+					</div>
 				</form>
         <button onClick={ this.props.triggerSearch } type="submit" className="btn btn-primary">Search</button>
         {/* <button onClick={ this.props.resetSearch } type="submit" className="btn btn-primary">Reset Search</button>*/}
