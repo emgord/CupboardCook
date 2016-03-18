c8bff605b75fefdc565a8c54545692edab449acb
emgord <emgord@gmail.com>
2016-03-17 17:24:56 -0700

    recipe search is reloading for some reason

--- app/assets/javascripts/components/recipe_search.js.jsx
+++ app/assets/javascripts/components/recipe_search.js.jsx
@@ -5,33 +5,45 @@ var RecipeSearch = React.createClass({
     if (this.props.heart) {
       var sortOptions =
       <div className = "btn-group">
-        <a onClick={this.props.showHeart} className="btn btn-primary disabled"><i className="fa fa-heart"></i>    Favorite Recipes</a>
-        <a onClick={this.props.showAll} className="btn btn-default">Browse All</a>
+        <a onClick={this.props.showHeart} className="btn btn-primary disabled btn-lg"><i className="fa fa-heart fa-lg"></i></a>
+        <a onClick={this.props.showAll} className="btn btn-default btn-lg">All</a>
       </div>;
     } else {
       var sortOptions =
           <div>
           <div className = "btn-group">
-            <a onClick={this.props.showHeart} className="btn btn-primary"><i className="fa fa-heart"></i>    Favorite Recipes</a>
-            <a className="btn btn-default disabled">Browse All</a>
+            <a onClick={this.props.showHeart} className="btn btn-primary btn-lg"><i className="fa fa-heart fa-lg"></i></a>
+            <a className="btn btn-default disabled btn-lg">All</a>
           </div>
       		<div className="btn-group">
             <a onClick={this.props.decrementMissing} className="btn btn-primary"><i className="fa fa-chevron-left"></i></a>
             <p className= "btn btn-default">{this.props.missing}</p>
             <a onClick={this.props.incrementMissing} className="btn btn-primary"><i className="fa fa-chevron-right"></i></a>
-            </div>
+          </div>
           </div>;
     }
 
 
 		return (
-        <div className = "recipe-search-box">
-          {sortOptions}
-					<RecipeSearchBox updateQuery={this.props.updateQuery}
-													 triggerSearch={this.props.triggerSearch}
-						 							 query={this.props.query}
-													 resetSearch={this.props.resetSearch}/>
-        </div>
+        <div className="container">
+					<div className="row search-box">
+						<div className="col-sm-12 search-box">
+						<div className="col-xs-12 col-sm-8 col-sm-push-2 col-md-6 col-md-push-3">
+							<h2>Search Recipes:</h2>
+							{sortOptions}
+							<RecipeSearchBox updateQuery={this.props.updateQuery}
+															 triggerSearch={this.props.triggerSearch}
+									 						 query={this.props.query}
+															 resetSearch={this.props.resetSearch}/>
+						</div>
+						</div>
+					</div>
+				</div>
+
+
+
+
+
 
 			);
 	}
--- app/assets/javascripts/components/recipe_search_box.js.jsx
+++ app/assets/javascripts/components/recipe_search_box.js.jsx
@@ -3,20 +3,19 @@ var RecipeSearchBox = React.createClass({
 	render () {
 		return (
 			<div className="recipe-search-box">
-				<form className="uk-form" ref="form" acceptCharset="UTF-8">
-					<div className="uk-form-icon">
-						<i className="uk-icon-search"></i>
+				<form className="form-inline" ref="form" acceptCharset="UTF-8">
+					<div className="form-group">
 						<input ref="query"
-									 className="uk-form-large uk-form-width-large"
+									 className="form-control"
 							 		 autoFocus="true"
+									 type="text"
 									 name="query"
 		               onChange={ this.props.updateQuery }
 									 placeholder="Find Recipes"
 										/>
 					</div>
+					<button onClick={ this.props.triggerSearch } type="submit" className="btn btn-primary">Search</button>
 				</form>
-        <button onClick={ this.props.triggerSearch } type="submit" className="btn btn-primary">Search</button>
-        {/* <button onClick={ this.props.resetSearch } type="submit" className="btn btn-primary">Reset Search</button>*/}
 			</div>
 			);
 	}
