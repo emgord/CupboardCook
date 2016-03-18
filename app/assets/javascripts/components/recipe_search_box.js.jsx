3fef99edee70c6f077217bf76b29a1069cc4a98d
emgord <emgord@gmail.com>
2016-03-17 19:17:45 -0700

    recipe search looks good

--- app/assets/javascripts/components/recipe_search_box.js.jsx
+++ app/assets/javascripts/components/recipe_search_box.js.jsx
@@ -2,24 +2,22 @@ var RecipeSearchBox = React.createClass({
 
 	render () {
 		return (
-			<div className="row">
-				<div className="col-lg-6">
+			<div className="col-xs-12 col-sm-8 col-sm-push-2 col-md-6 col-md-push-3">
+				<form ref="form" acceptCharset="UTF-8">
 					<div className="input-group">
-						<form ref="form" acceptCharset="UTF-8">
-						<p><input ref="query"
-										className="form-control"
-										autoFocus="true"
-										name="query"
-										type="text"
-										onChange={ this.props.updateQuery }
-										placeholder="Find Recipes"
-										/></p>
-						<span class="input-group-btn">
-							<button onClick={ this.props.triggerSearch } type="button" className="btn btn-primary">Search</button>
+						<input ref="query"
+											className="form-control"
+										  autoFocus="true"
+										  name="query"
+										  type="text"
+										  onChange={ this.props.updateQuery }
+										  placeholder="Find Recipes"
+										/>
+						<span className="input-group-btn">
+				 			<button onClick={ this.props.triggerSearch } type="button" className="btn btn-primary"><i className="fa fa-search"></i></button>
 						</span>
-						</form>
 					</div>
-				</div>
+				</form>
 			</div>
 			);
 	}
