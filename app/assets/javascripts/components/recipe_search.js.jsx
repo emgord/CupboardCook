fe17196a5be8a7998005e8603f88a949530f107f
emgord <emgord@gmail.com>
2016-03-17 20:41:11 -0700

    recipe search form complete

--- app/assets/javascripts/components/recipe_search.js.jsx
+++ app/assets/javascripts/components/recipe_search.js.jsx
@@ -1,26 +1,45 @@
 var RecipeSearch = React.createClass({
 
 	render () {
-
     if (this.props.heart) {
       var sortOptions =
-      <div className = "btn-group">
-        <a onClick={this.props.showHeart} className="btn btn-primary disabled"><i className="fa fa-heart"></i>    Favorite Recipes</a>
-        <a onClick={this.props.showAll} className="btn btn-default">All</a>
-      </div>;
+			<div className="row">
+				<div className="col-xs-12 col-sm-8 col-sm-push-2 col-md-6 col-md-push-3">
+					<span>Search within:</span>
+		      <div className="btn-group">
+		        <a onClick={this.props.showHeart} className="btn btn-primary disabled"><i className="fa fa-heart"></i></a>
+		        <a onClick={this.props.showAll} className="btn btn-default">All</a>
+		      </div>
+				</div>
+				<br/>
+			</div>;
     } else {
       var sortOptions =
-          <div>
-          <div className = "btn-group">
-            <a onClick={this.props.showHeart} className="btn btn-primary"><i className="fa fa-heart"></i></a>
-            <a className="btn btn-default disabled">All</a>
-          </div>
-      		<div className="btn-group">
-            <a onClick={this.props.decrementMissing} className="btn btn-primary"><i className="fa fa-chevron-left"></i></a>
-            <p className= "btn btn-default">{this.props.missing}</p>
-            <a onClick={this.props.incrementMissing} className="btn btn-primary"><i className="fa fa-chevron-right"></i></a>
-          </div>
-          </div>;
+			<div>
+        <div className="row">
+					<div className="col-xs-12 col-sm-8 col-sm-push-2 col-md-6 col-md-push-3">
+							<span className="form-text">Search within:</span>
+							<div className="btn-group">
+							<a onClick={this.props.showHeart} className="btn btn-primary"><i className="fa fa-heart"></i></a>
+							<a className="btn btn-default disabled">All</a>
+							</div>
+						</div>
+				</div>
+				<br/>
+				<div className="row">
+					<div className="col-xs-12 col-sm-8 col-sm-push-2 col-md-6 col-md-push-3">
+						<span className="form-text">Missing Ingredients:</span>
+						  <div className="btn-group">
+						    <a onClick={this.props.decrementMissing} className="btn btn-primary"><i className="fa fa-chevron-left"></i></a>
+						    <p className= "btn btn-default">{this.props.missing}</p>
+						    <a onClick={this.props.incrementMissing} className="btn btn-primary"><i className="fa fa-chevron-right"></i></a>
+						    </div>
+							</div>
+						</div>
+					<br/>
+				</div>
+
+      ;
     }
 
 
